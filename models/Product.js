cef4e0482795509d7180ca746a47f95c05c99340
Samihat <sameerah11@hotmail.com>
2021-10-26 22:17:52 -0400

    debugging the models

--- models/Product.js
+++ models/Product.js
@@ -38,10 +38,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-    },
-    references: {
-      model: "category",
-      key: "id",
+      references: {
+        model: "category",
+        key: "id",
+      },
     },
   },
   {
